2d4034d1d0fd9d84864f17aea8e3e85951e2cf06
Oscar0807-z <oscarz43@nycstudents.net>
2026-02-02 08:45:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,21 @@ import turtle
 from turtle import *
 t = Turtle()
 
-t.forward(100)
 
+
+
+t.shape('turtle')
+""" def triangle(x):
+    for i in range (3):
+        t.forward(125)
+        t.left(120)
+
+triangle(200) """
+def square (x):
+    def addSquares(iRange):
+        length = 25
+        for i in range(iRange):
+            square(5, 90)
+        length += 5
+addSquares(60)
 turtle.done()
